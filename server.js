df44659d4f5014781c6ee79366647e8e449be3e5
Luis G <luisdavidnazareth@gmail.com>
2024-10-16 22:29:32 -0300

    creamos las funciones de middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import "./config/database.js"//este el archico que importamos desde database.js
 import cors from 'cors'
 import morgan from "morgan";
 import indexRouter from './router/index.js'
+import not_found_handler from "./middlewares/not_found_handler.js";
+import error_handler from "./middlewares/error_handler.js";
 
 const server = express()
 
@@ -14,12 +16,16 @@ const PORT = process.env.PORT || 8080
 const ready = ()=> console.log("server ready in port :"+PORT);
 
 server.use(express.json())//permite trabajar con formato json en entrada y salida
-server.use(express.urlencoded({ extended: true }))
-server.use(cors())
-server.use(morgan('dev'))
+server.use(express.urlencoded({ extended: true }))//permite capturar consultas complejas
+server.use(cors())//para permitir origenes cruzadas
+server.use(morgan('dev'))//para registrar peticiones HTTP
+
+
 
 //router
 server.use('/api',indexRouter)
+server.use(not_found_handler)
+server.use(error_handler)
 
 server.listen(PORT,ready)
 
